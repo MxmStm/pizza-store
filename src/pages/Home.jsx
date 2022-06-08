d58c805ecb74dfb06490b68409d265bf30018f48
Maxim <maksstoma@gmail.com>
2022-06-08 19:51:17 +0300

    added search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,27 +5,34 @@ import {Sort} from "../components/Sort";
 import {Skeleton} from "../components/PizzaBlock/Skeleton";
 import {PizzaBlock} from "../components/PizzaBlock/PizzaBlock";
 
-export const Home = () => {
+export const Home = ({searchValue}) => {
     const [pizzas, setPizzas] = useState([])
     const [isLoading, setIsLoading] = useState(true)
     const [categoryId, setCategoryId] = useState(0)
     const [sortType, setSortType] = useState({
-        name: 'популярности', sortProperty: 'rating'
+        name: 'популярности',
+        sortProperty: 'rating'
     })
 
-    const category = categoryId > 0 ? `category=${categoryId}` : ''
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const search = searchValue ? `search=${searchValue}` : '';
 
     useEffect(() => {
         setIsLoading(true)
         axios.get(
             `https://62892e0a7af826e39e69750f.mockapi.io/items?${
-                category}&sortBy=${sortType.sortProperty}`)
+                category}&sortBy=${sortType.sortProperty}&${search}`)
             .then(res => {
                 setIsLoading(false)
                 setPizzas(res.data)
             })
         window.scroll(0, 0)
-    }, [category, categoryId, sortType.sortProperty])
+    }, [category, categoryId, search, sortType.sortProperty])
+
+    const pizzasList = pizzas.map(pizza =>
+        <PizzaBlock key={pizza.id} {...pizza}/>)
+    const skeletons = [...new Array(8)].map((_, index) =>
+        <Skeleton key={index}/>)
 
     return (
         <div className="container">
@@ -41,10 +48,7 @@ export const Home = () => {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-                {isLoading
-                    ? [...new Array(8)].map((_, index) => <Skeleton key={index}/>)
-                    : pizzas.map(pizza => <PizzaBlock
-                        key={pizza.id} {...pizza}/>)}
+                {isLoading ? skeletons : pizzasList}
             </div>
         </div>
     );
