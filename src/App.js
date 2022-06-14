ad5fe5364b8ac2b6154cd2a5f8c3d2331a6e7ce3
Maxim <maksstoma@gmail.com>
2022-06-14 17:11:38 +0300

    added Outlet

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 import './scss/app.scss';
-import {Header} from "./components/Header";
 import {Home} from "./pages/Home";
 import {Cart} from "./pages/Cart";
 import {NotFound} from "./pages/NotFound";
 import {FullPizza} from "./pages/FullPizza";
+import {MainLayout} from "./layouts/MainLayout";
 
 function App() {
     return (
-        <div className="wrapper">
-            <Header/>
-            <div className="content">
-                <Routes>
-                    <Route path={'/pizza-store'}
-                           element={<Navigate to={'/'}/>}/>
-                    <Route path={'/'}
-                           element={<Home/>}/>
-                    <Route path={'/cart'} element={<Cart/>}/>
-                    <Route path={'/pizza/:id'} element={<FullPizza/>}/>
-                    <Route path={'*'} element={<NotFound/>}/>
-                </Routes>
-            </div>
-        </div>
+        <Routes>
+            <Route path={'/'} element={<MainLayout/>}>
+                <Route path={'pizza-store'}
+                       element={<Navigate to={'/'}/>}/>
+                <Route path={''}
+                       element={<Home/>}/>
+                <Route path={'cart'} element={<Cart/>}/>
+                <Route path={'pizza/:id'} element={<FullPizza/>}/>
+                <Route path={'*'} element={<NotFound/>}/>
+            </Route>
+        </Routes>
     );
 }
 
--- src/pages/FullPizza.jsx
+++ src/pages/FullPizza.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 
 export const FullPizza = () => {
     const [pizza, setPizza] = useState()
     const {id} = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
         async function fetchPizza() {
@@ -13,6 +14,7 @@ export const FullPizza = () => {
                 setPizza(res.data)
             } catch (error) {
                 alert('Ошибка запроса пиццы')
+                navigate('/')
             }
         }
 
